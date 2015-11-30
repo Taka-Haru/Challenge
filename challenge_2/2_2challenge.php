8508f60d07f73bb6f3a6e2e067664438bba388a4
TakaHaru <xt.sakamoto@gmail.com>
2015-11-30 10:07:44 +0900

    ラジオボタンでの復習

--- challenge_2/2_2challenge.php
+++ challenge_2/2_2challenge.php
@@ -1,72 +1,3 @@
-<?php
-/*１０．クエリストリングを利用して、以下の処理を実現してください。
-
-　簡易素因数分解のロジックを作成します。
-　クエリストリングから渡された数値を1ケタの素数で可能な限り分解し、
-　元の値と素因数分解の結果を表示するようにしてください。
-　2ケタ以上の素数が含まれた数値の場合は、
-　「元の値　1ケタの素因数　その他」と表記して、その他　に含んでください。
-*/
-
-    //分解数値を取得
-    $num = $_GET['num'];
-    //初期値を取っておく
-    $shokichi = $num;
-
-    //素数カウント用変数　(連想配列)
-    $counter = array(
-                    2 => 0,           //キーワード=>データ
-                    3 => 0,
-                    5 => 0,
-                    7 => 0,
-                    );
-    //マイナス値は除外
-    if($num <= 0){
-      echo 'マイナス値は処理できません';
-      exit;
-    }
-
-    //２の可能性を確認
-    if(($num % 2) == 0){          //もし２で割ってあまりが０の時
-      //２で割れるので、処理
-      while($num % 2 == 0){     //記入値を２で割ったらあまり０になる場合は繰り返す
-      //割れるので、割ってカウント
-      $num = $num / 2;          //繰り返す処理
-      $counter[2]++;            //繰り返す処理
-    }                             //キーワード「２」に格納されているデータ「０」に
-  }                               //++(インクリメント)処理していく
-
-    //3の可能性を確認
-    if(($num % 3) == 0){
-      //3で割れるので、処理
-      while(($num % 3) == 0){
-      //割れるので、割ってカウント
-      $num = $num / 3;
-      $counter[3]++;
-      }
-    }
-
-    //5の可能性を確認
-    if(($num % 5) == 0){
-      //5で割れるので、処理
-      while(($num % 5) == 0){
-      //割れるので、割ってカウント
-      $num = $num / 5;
-      $counter[5]++;
-      }
-    }
-
-    //7の可能性を確認
-    if(($num % 7) == 0){
-      //7で割れるので、処理
-      while(($num % 7) == 0){
-      //割れるので、割ってカウント
-      $num = $num / 7;
-      $counter[7]++;
-      }
-    }
-?>
-
 <!DOCTYPE html>
 <html lang="ja">
 
@@ -75,20 +6,46 @@
       <title>2_2challenge</title>
 </head>
   <body>
+  <!--
+  ２．swtich文を利用して、以下の処理を実現してください。
+  　値が’A’なら「英語」、値が’あ’なら「日本語」、それ以外は何も表示しない処理
+  (ラジオボタンでの選択結果で表示の復習)
+  -->
 
-<?php
-  echo '元の値：'.$shokichi.'<br>';                   //foreach文で連想配列からキーと値を同時に取り出す
-  foreach($counter as $key => $value){              //変数「$key」に連想配列のキーが格納される。　→　キーと関連づけられた値を$valueに格納する。
-    if ($value>0){                                  //もし、インクリメントされた回数が0以上なら
-      echo '素因数：'.'「'.$key.'」'.' の'.$value.'乗<br>';                //foreach文なので「2,3,5,7」ループ処理を実行してくれる
-      }
-    }            //foreach文はここで終了
-  if($num > 1){
-    //１以上の場合、割り切れなかった
-    echo 'その他：'.$num;
-}
-?>
+  <!--問題定義-->
+  チェックした項目が何の言語なのか表示します。<br>
+  ※「それ以外」は何も表示しません。<br><br>
 
-</body>
 
+  <form action=2_2challenge.php method="POST">
+    　Ａ　　<input type="radio" name=rdo value="英語"><br>
+    　あ　　<input type="radio" name=rdo value="日本語"><br>
+  その他　<input type="radio" name=rdo value="それ以外"><br>
+  <!--何も選択がない場合にエラー表記が出ないようにする。-->
+  <input type="radio" name=rdo value="" checked="checked" style="display:none;">
+    <br>
+    <input type="submit" name="btn" value="言語">
+  </form>
+  <br>
+
+  <?php
+    if(!empty($_POST['btn'])){
+      $x = $_POST['rdo'];
+      switch($x){
+        case '英語':
+          echo '英語';
+          break;
+        case '日本語':
+          echo '日本語';
+          break;
+        case '';
+          echo '何か選んで下さい。';
+          break;
+        default:
+          echo '';
+        }
+    }
+  ?>
+
+  </body>
 </html>
