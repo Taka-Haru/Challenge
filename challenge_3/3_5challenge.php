e940f6a855f4129d7e596e73ad937a1ca083925e
TakaHaru <xt.sakamoto@gmail.com>
2015-12-10 19:45:13 +0900

    (復習)配列の中身変更

--- challenge_3/3_5challenge.php
+++ challenge_3/3_5challenge.php
@@ -3,7 +3,7 @@
 
 <head>
 <meta charset="UTF-8">
-      <title>PHP KADAI1</title>
+      <title>3_5challenge</title>
 </head>
   <body>
 <?php
@@ -13,19 +13,26 @@ id(数値),名前,生年月日、住所を返却し、受け取った後はid以
 */
 
     function get_members(){
-      echo "天下統一してみました！！<br>";
+      $id=2;
+      $name='豊臣秀吉';
+      $birthday='2月6日';
+      $add='尾張国';
 
-      $id=2;$name='豊臣秀吉';$birth='2月6日';$add='尾張国';
-      return array('A'=>$id,'B'=>$name,'C'=>$birth,'D'=>$add);
+      return array(
+                1 => $id,
+                2 => $name,
+                3 => $birthday,
+                4 => $add
+      );
     }
 
-    $yobidashi =  get_members();
-    foreach($yobidashi as $key => $value){
-      if($key == 'A'){
+    $profile =  get_members();
+    foreach($profile as $key => $value){
+      if($key == 1){
         continue;
       }else
-        echo "$value<br>";
-      }
+        echo $value.'<br>';
+    }
 ?>
 </body>
 
