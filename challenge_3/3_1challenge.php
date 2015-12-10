df4a43abf944247142d7984935b5ba076c4a118a
TakaHaru <xt.sakamoto@gmail.com>
2015-12-10 19:32:57 +0900

    (復習)submitで実行

--- challenge_3/3_1challenge.php
+++ challenge_3/3_1challenge.php
@@ -3,24 +3,35 @@
 
 <head>
 <meta charset="UTF-8">
-      <title>PHP KADAI1</title>
+      <title>3_1challenge</title>
 </head>
+  ボタンを押してください
+  <br><br>
+  <form action="http://localhost/develop/challenge3/3_1challenge.php" method="POST">
+    <input type="submit" name="btn" value="プロフィール">
+  </form>
+
   <body>
-<?php
 
-/*課題1:自分のプロフィール(名前、生年月日、自己紹介)を3行に分けて表示するユーザー定義関数を作り
-      、関数を2回呼び出して表示して下さい
-*/
-
+    <?php
       function my_profile(){
-          echo "TakaharuSakamoto<br>";
-          echo "1988-11-24<br>";
-          echo "前職：飲食業<br>";
+          return array(
+                1 => 'TakaharuSakamoto<br>',
+                2 => '1988-11-24<br>',
+                3 => '前職：飲食業<br>',
+          );
       }
-      my_profile();
-      my_profile();
-      echo '<br>';
-?>
-</body>
+
+      if(!empty($_POST['btn'])){
+        for($i=1;$i<=2;$i++){
+          $profile = my_profile();
+          foreach($profile as $value){
+          echo $value;
+          }
+        }
+      }
+    ?>
+
+  </body>
 
 </html>
