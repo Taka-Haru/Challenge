1d01c61384788c1157a48316579b84c49ac366b9
TakaHaru <xt.sakamoto@gmail.com>
2015-12-11 00:41:15 +0900

    (復習)input文修正

--- challenge_5/5_1challenge.php
+++ challenge_5/5_1challenge.php
@@ -8,13 +8,14 @@
 １．以下の入力フィールドを持った入力データをPHPで処理できるHTMLを記述してください。
 ・名前（テキストボックス）、性別（ラジオボタン）、趣味（複数行テキストボックス）
 -->
-    <form action="5_2challenge.php" method="POST">
+    <form action="./5_2challenge.php" method="POST">
       名前<br>
       <input type="text" name="txtName">
       <br><br>
       性別<br>
       男<input type="radio" name="rdo" value="男">
       女<input type="radio" name="rdo" value="女"><br>
+        <input type="radio" name="rdo" value="" checked="checked" style="display:none;">
       <br>
       趣味・特技:<br>
       <textarea name="mulText" rows="4" cols="40"></textarea><br>
