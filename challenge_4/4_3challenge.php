c1aff6aa044c5e6b5c5959b8e0d27a2530f11635
TakaHaru <xt.sakamoto@gmail.com>
2015-12-11 00:25:07 +0900

    (復習)提出

--- challenge_4/4_3challenge.php
+++ challenge_4/4_3challenge.php
@@ -3,21 +3,18 @@
 
 <head>
 <meta charset="UTF-8">
-      <title>PHP KADAI1</title>
+      <title>4_3challenge</title>
 </head>
   <body>
-<?php
+    <!-- ３．2016年11月4日 10時0分0秒のタイムスタンプを作成し、
+    　　「年-月-日 時:分:秒」で表示してください。 -->
+    <?php
 
-/*
-３．2016年11月4日 10時0分0秒のタイムスタンプを作成し、
-　　「年-月-日 時:分:秒」で表示してください。
-*/
-$timestamp = mktime(10, 00, 00, 11, 4, 2016);
+    $timestamp = mktime(10, 00, 00, 11, 4, 2016);
+    $newday = date('Y-m-d H:i:s',$timestamp);
+    echo $newday;
 
-$newday = date('Y-m-d H:i:s',$timestamp);
-echo $newday;
-
-?>
+    ?>
 </body>
 
 </html>
