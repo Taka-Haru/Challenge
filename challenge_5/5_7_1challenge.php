00e973ba18028624558dd4b475bf8602cdddb373
TakaHaru <xt.sakamoto@gmail.com>
2015-12-11 01:01:44 +0900

    (要復習)history.back方式

--- challenge_5/5_7_1challenge.php
+++ challenge_5/5_7_1challenge.php
@@ -4,19 +4,14 @@
     <title>5_7_1challenge</title>
   </head>
   <body>
-<!--
-７．以下の機能を実装してください。
-　
-名前・性別・趣味を入力するページを作成してください。
-また、入力された名前・性別・趣味を記憶し、次にアクセスした際に
-記録されたデータを初期値として表示してください。
-※PHPと同時に、HTMLの知識が必要になります。
-※入力フィールドの使い方を調べてみましょう。
-
-配列を使ったやり方もあり。
-
--->
-
+    <!--
+    ７．以下の機能を実装してください。
+    名前・性別・趣味を入力するページを作成してください。
+    また、入力された名前・性別・趣味を記憶し、次にアクセスした際に
+    記録されたデータを初期値として表示してください。
+    ※PHPと同時に、HTMLの知識が必要になります。
+    ※入力フィールドの使い方を調べてみましょう。
+    -->
     <form action="5_7_2challenge.php" method="POST">
       名前<br>
       <input type="text" name="txtName">
@@ -26,6 +21,7 @@
       性別<br>
       男<input type="radio" name="rdoGender" value="男"> <br>
       女<input type="radio" name="rdoGender" value="女"> <br>
+        <input type="radio" name="rdoGender" value="" checked="checked" style="display:none;">
       <br>
       趣味<br>
       <textarea name="mulText" rows="4" cols="40" value="趣味"></textarea><br>
@@ -33,7 +29,6 @@
       <br>
       <input type="submit" name="btnSubmit" value="GO!!!!"style="width:250px">
     </form>
-
 
   </body>
 </html>
--- challenge_5/5_7_2challenge.php
+++ challenge_5/5_7_2challenge.php
@@ -5,43 +5,38 @@
   </head>
   <body>
 
-<?php
-//まず、ここでまとめてついかしてしまう！
-$name = $_POST['txtName'];
-$gender = $_POST['rdoGender'];
-$hobby = $_POST['mulText'];
+    <?php
+    //まず、ここでまとめてついかしてしまう！
+    $name = $_POST['txtName'];
+    $gender = $_POST['rdoGender'];
+    $hobby = $_POST['mulText'];
 
-if($name == null){
-  echo '名前が記入されていません。';
-}else{
-  echo 'はじめまして、 '.$name.'様';
-  }
-echo '<br>';
+    if($name == null){
+      echo '名前が記入されていません。';
+    }else{
+      echo 'はじめまして、 '.$name.'様';
+    }
+    echo '<br>';
 
-/*　ラジオボタンの時のif文が分からない。。。エラーになる
-    　男女の選択にチェックを入れないとエラー文がでる。
-if($gender== null){
-  echo '性別が選択されていません。';
-  }
-*/
+    if($gender == null){
+      echo '性別が選択されていません。';
+    }else{
+    echo 'あ、'.$gender.'の人だったんですね！！';
+    }
+    echo '<br>';
+    if($hobby == null){
+      echo '趣味が記入されていません。';
+    }else{
+      echo 'へ〜、'.$hobby.'が好きなんですね♫';
+    }
+    echo '<br>';
+    echo '<br>';
 
-echo 'あ、'.$gender.'の人だったんですね！！';
+    //Javascriptの「onclick="history.back()"」だったら簡単にできるという。。。
+    echo '<form>';
+    echo '<input type="button" onclick="history.back()" value="戻る">';
+    echo '</form>';
 
-echo '<br>';
-if($hobby == null){
-  echo '趣味が記入されていません。';
-}else{
-  echo 'へ〜、'.$hobby.'が好きなんですね♫';
-  }
-echo '<br>';
-echo '<br>';
-
-//Javascriptの「onclick="history.back()"」だったら簡単にできるという。。。反則ですが。。
-echo '<form>';
-echo '<input type="button" onclick="history.back()" value="戻る">';
-echo '</form>';
-
-
-?>
+    ?>
   </body>
 </html>
