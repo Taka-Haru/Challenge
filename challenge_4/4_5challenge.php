e5883618bc603d5c5a4fdfb4d3f1ff217c2ea9d0
TakaHaru <xt.sakamoto@gmail.com>
2015-12-11 00:29:13 +0900

    要変更(自由な文字列を判断できる様に)

--- challenge_4/4_5challenge.php
+++ challenge_4/4_5challenge.php
@@ -3,19 +3,19 @@
 
 <head>
 <meta charset="UTF-8">
-      <title>PHP KADAI1</title>
+      <title>4_5challenge</title>
 </head>
   <body>
-<?php
+    <!-- ５．自分の氏名について、バイト数と文字数を取得して、表示してください。 -->
+  <?php
 
-/*
-５．自分の氏名について、バイト数と文字数を取得して、表示してください。
-*/
-echo strlen ('武田信玄');
-echo '<br>';
-echo mb_strlen('上杉謙信');
-echo '<br>';
-?>
+  echo '『武田信玄』の名前のバイト数は：';
+  echo strlen ('武田信玄');
+  echo '<br>';
+  echo '『上杉謙信』の名前の文字数は：';
+  echo mb_strlen('上杉謙信');
+  echo '<br>';
+  ?>
 </body>
 
 </html>
