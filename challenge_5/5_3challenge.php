0f2ed6084f6c704adb0918f192fbe6950a9bfdc2
TakaHaru <xt.sakamoto@gmail.com>
2015-12-11 00:42:35 +0900

    (復習)提出

--- challenge_5/5_3challenge.php
+++ challenge_5/5_3challenge.php
@@ -5,34 +5,21 @@
 <meta charset="UTF-8">
       <title>5_3challenge</title>
 </head>
+    <!-- ３．クッキーに現在時刻を記録し、次にアクセスした際に、
+    前回記録した日時を表示してください。 -->
   <body>
-<?php
+    <?php
+      if(!isset($_COOKIE['LastLoginDate'] )) {
+        echo '初回だから記録がありません';
+      }else{
+        $lastDate = $_COOKIE['LastLoginDate'];
+        echo '前回ログイン時は、'.$lastDate.'です！';
+      };
 
-/*３．クッキーに現在時刻を記録し、次にアクセスした際に、
-前回記録した日時を表示してください。
-*/
-
-//前回ログイン時刻を表示 or 初回エラー表示
-if(!isset($_COOKIE['LastLoginDate'] )) {
-  echo '初回だから記録がありません';
-}else{
-  $lastDate = $_COOKIE['LastLoginDate'];
-  echo '前回ログイン時は、'.$lastDate.'です！';
-};
-
-//ログイン時刻を記録
-$access_time = date('H時i分s秒');
-setcookie('LastLoginDate',$access_time);
-
-
-echo '<br>';
-
-
-
-
-
-
-?>
-</body>
+      //ログイン時刻を記録
+      $access_time = date('H時i分s秒');
+      setcookie('LastLoginDate',$access_time);
+    ?>
+  </body>
 
 </html>
