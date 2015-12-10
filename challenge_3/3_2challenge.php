8d8d0b363676e0280f3e67818095407202c2e750
TakaHaru <xt.sakamoto@gmail.com>
2015-12-10 19:33:52 +0900

    (復習)好きな数字入力

--- challenge_3/3_2challenge.php
+++ challenge_3/3_2challenge.php
@@ -3,26 +3,37 @@
 
 <head>
 <meta charset="UTF-8">
-      <title>PHP KADAI1</title>
+      <title>3_2challenge</title>
 </head>
-  <body>
-<?php
+  好きな数字を入力すると、<br>
+  入力された数値が、偶数か奇数かを判定します。
+  <form action="http://localhost/develop/challenge3/3_2challenge.php" method="POST">
+    <input type="text" name="num" size="7">
+    <input type="submit" name="btn" value="奇数 or 偶数">
+    <br><br>
+  </form>
 
-/*課題2:引数として適当な数値を受け取り、その値が奇数か偶数か判別＆表示する処理を関数として制作し、
-      実行する
-*/
+  <body>
+    <?php
 
       function view_int($num){
         if($num % 2 == 0){
-            echo "偶数です";
+            echo "偶数です。";
         }else{
-            echo "奇数です";
+            echo "奇数です。";
           }
       }
-      view_int(3);
-      echo '<br>';
 
-?>
-</body>
+      if(!empty($_POST['btn'])){
+        if(($_POST['num'] =='')){
+          echo '何か数値を入力して下さい。';
+        }else{
+        $x = $_POST['num'];
+        view_int($x);
+        }
+      }
+
+    ?>
+  </body>
 
 </html>
