20161434e0d29dbd7001151e9633ad3d124d13a2
TakaHaru <xt.sakamoto@gmail.com>
2015-12-10 19:40:32 +0900

    (復習)カウント数表示

--- challenge_3/3_7challenge.php
+++ challenge_3/3_7challenge.php
@@ -3,31 +3,29 @@
 
 <head>
 <meta charset="UTF-8">
-      <title>PHP KADAI1</title>
+      <title>3_7challenge</title>
 </head>
   <body>
-<?php
+    <!-- 課題7:初期値3のグローバルな値を2倍していく処理の関数と、
+          staticとしてその関数が何回実行されたのかを保持する変数を設ける。
+          この関数を20回呼び出す -->
+    <?php
 
-/*
-課題7:初期値3のグローバルな値を2倍していく処理の関数と、
-      staticとしてその関数が何回実行されたのかを保持する変数を設ける。
-      この関数を20回呼び出す
-*/
-$global_number=3;
+    $global_number=3;
 
-    function check_number(){
-      static $local_number=1;     //この関数内での処理をキープする。
-      global $global_number;      //global宣言で$lobal_number現実世界から引張てくる
-      $global_number = $global_number * 2;
-      echo "$global_number ,";
-      $local_number+=1;       //実行回数のカウント
-    }
+      function check_number(){
+        static $local_number=0;
+        global $global_number;
+        $global_number = $global_number * 2;
+        $local_number+=1;       //実行回数のカウント
+        echo $local_number.'回目：'.$global_number.'<br>';
+      }
 
-    for($i=0;$i<20;$i++){
-    check_number() * $i;
-  }
+      for($i=0;$i<20;$i++){
+        check_number() * $i;
+      }
 
-?>
-</body>
+    ?>
+  </body>
 
 </html>
