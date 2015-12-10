227e3cf4785b9626513342304d954c93ba6a63da
TakaHaru <xt.sakamoto@gmail.com>
2015-12-11 00:43:39 +0900

    (復習)アクセス回数カウント表示

--- challenge_5/5_4challenge.php
+++ challenge_5/5_4challenge.php
@@ -5,29 +5,40 @@
 <meta charset="UTF-8">
       <title>5_2challenge</title>
 </head>
+    <!-- ４．３と同じ機能をセッションで作成してください。
+    (クッキーに現在時刻を記録し、次にアクセスした際に、前回記録した日時を表示してください) -->
+    <?php
+      //セッション開始
+      session_start();
+
+      //セッションから時刻データを取り出す
+      if(!isset($_SESSION['access_time'])){
+        echo '初回なのでデータがありません。';
+      }else{
+        $lastDate = $_SESSION['access_time'];
+        echo '前回ログイン時刻は'.$lastDate.'です！';
+        echo '<br>';
+      }
+
+      //セッションから回数データを取り出す
+      $count = 1;
+      if(isset($_SESSION['access_count'])){
+        $count = $_SESSION['access_count'];
+        if($count == 1){
+          echo '初回アクセスです';
+          $count++;
+        }else{
+          echo $count.'回目のアクセスです。';
+          $count++;
+        }
+      }
+
+      //セッションに情報を入れる
+      $_SESSION['access_time']=date('H時i分s秒');
+      $_SESSION['access_count']=$count;
+    ?>
+
   <body>
-<?php
 
-/*４．３と同じ機能をセッションで作成してください。
-(クッキーに現在時刻を記録し、次にアクセスした際に、
-前回記録した日時を表示してください)
-*/
-//セッション開始
-session_start();
-
-//セッションからデータを取り出す
-if(!isset($_SESSION['time'])){
-  echo '初回なのでデータがありません。';
-}else{
-  $lastDate = $_SESSION['time'];
-  echo '前回ログイン時刻は'.$lastDate.'です！';
-};
-
-//セッションに情報を入れる
-$_SESSION['time']=date('H時i分s秒');
-
-
-?>
-</body>
-
+  </body>
 </html>
