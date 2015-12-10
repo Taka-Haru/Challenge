0fd81d839a80c82c00e0817fc957d361f78e1515
TakaHaru <xt.sakamoto@gmail.com>
2015-12-11 00:10:57 +0900

    (復習)色々なパターン選択式

--- challenge_3/3_3challenge.php
+++ challenge_3/3_3challenge.php
@@ -5,6 +5,11 @@
 <meta charset="UTF-8">
       <title>3_3challenge</title>
 </head>
+<!-- 課題3:引き数が3つの関数を定義する。1つ目は適当な数値を、2つ目はデフォルト値が5の数値を、
+最後はデフォルト値がfalseの$typeを引き数として定義する。
+1つ目の引き数に2つ目の引き数を掛ける計算をする関数を作成し、
+$typeがfalseの時はその値を表示、trueのときはさらに累乗して表示する。 -->
+
   <form action="./3_3challenge.php" method="POST">
     <input type="text" name="no1" size="5" onKeyup="this.value=this.value.replace(/[^0-9]+/,'')">
     ×
@@ -16,13 +21,8 @@
     <br><br>
   </form>
   <body>
+
     <?php
-    /*
-    課題3:引き数が3つの関数を定義する。1つ目は適当な数値を、2つ目はデフォルト値が5の数値を、
-    最後はデフォルト値がfalseの$typeを引き数として定義する。
-    1つ目の引き数に2つ目の引き数を掛ける計算をする関数を作成し、
-    $typeがfalseの時はその値を表示、trueのときはさらに累乗して表示する。
-    */
 
    //function 関数名($引数1,$引数2,$引数3=デフォルト値)
       function show_data($num,$secondnum,$type=false){
