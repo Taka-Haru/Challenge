1ea977953954baf1805dfb781886825664aab27b
TakaHaru <xt.sakamoto@gmail.com>
2015-12-11 00:31:43 +0900

    (復習)提出

--- challenge_4/4_9challenge.php
+++ challenge_4/4_9challenge.php
@@ -3,21 +3,18 @@
 
 <head>
 <meta charset="UTF-8">
-      <title>PHP KADAI1</title>
+      <title>4_9challenge</title>
 </head>
   <body>
-<?php
+    <!-- ９．ファイルから自己紹介を読み出し、表示してください。-->
+  <?php
 
-/*
-９．ファイルから自己紹介を読み出し、表示してください。
-*/
+  $filename = fopen('text2.txt','r');
+  $file_txt = fgets($filename);
+  fclose($filename);
+  echo $file_txt
 
-$filename = fopen('text2.txt','r');
-$file_txt = fgets($filename);
-fclose($filename);
-
-echo $file_txt
-?>
+  ?>
 </body>
 
 </html>
