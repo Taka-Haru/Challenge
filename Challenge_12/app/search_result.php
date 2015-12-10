037f1d3cc77e62489aaa6dcc8f23430da43cf469
TakaHaru <xt.sakamoto@gmail.com>
2015-12-10 12:04:37 +0900

    工事中

---
+++ Challenge_12/app/search_result.php
@@ -0,0 +1,44 @@
+<?php
+require_once '../common/defineUtil.php';
+require_once '../common/scriptUtil.php';
+require_once '../common/dbaccesUtil.php';
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+      <title>検索結果画面</title>
+</head>
+    <body>
+        <h1>検索結果</h1>
+        <table border=1>
+            <tr>
+                <th>名前</th>
+                <th>生年月日</th>
+                <th>種別</th>
+                <th>登録日時</th>
+            </tr>
+        <?php
+        $result = null;
+        if(empty($_GET['name']) && empty($_GET['year']) && empty($_GET['type'])){
+            $result = serch_all_profiles();
+        }
+        if(!empty($_GET['name'])){
+            $result = search_profiles($_GET['name']);
+
+        }
+        foreach($result as $value){
+        ?>
+            <tr>
+                <td><a href="<?php echo RESULT_DETAIL ?>?id=<?php echo $value['userID']?>"><?php echo $value['name'];?></a></td>
+                <td><?php echo $value['birthday']; ?></td>
+                <td><?php echo ex_typenum($value['type']); ?></td>
+                <td><?php echo date('Y年n月j日　H時i分s秒', strtotime($value['newDate']));; ?></td>
+            </tr>
+        <?php
+        }
+    echo return_top();
+        ?>
+        </table>
+</body>
+</html>
