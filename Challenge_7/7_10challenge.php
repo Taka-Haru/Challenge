257887f217af81e3deaae70afb6a88d8ae7cef41
TakaHaru <xt.sakamoto@gmail.com>
2015-12-11 13:51:56 +0900

    (復習)提出

--- Challenge_7/7_10challenge.php
+++ Challenge_7/7_10challenge.php
@@ -15,35 +15,35 @@
   <br>
   <input type="submit" name="btnSubmit" value="削除!!">
 
-    <?php
+  <?php
 
-    //try-catchで接続エラーを取得＆表示
-    try{
-      $pdo_object=
-      new PDO('mysql:host=localhost;dbname=Challenge_db;charset=utf8',
-      'sakamoto',2591);
-    }catch(PDOException $E){
-      die('接続に失敗しました:'.$E->getMessage());
-    }
+  //try-catchで接続エラーを取得＆表示
+  try{
+    $pdo_object=
+    new PDO('mysql:host=localhost;dbname=Challenge_db;charset=utf8',
+    'sakamoto',2591);
+  }catch(PDOException $E){
+    die('接続に失敗しました:'.$E->getMessage());
+  }
 
-    if(isset ($_POST['profilesID'])){
-      $id = $_POST['profilesID'];
-    }else{
-      $id = null;
-    }
+  if(isset ($_POST['profilesID'])){
+    $id = $_POST['profilesID'];
+  }else{
+    $id = null;
+  }
 
-    //SQL文を格納した文字列を定義(今回は指定レコードを削除するSQL文)
-    $sql = "delete from profiles where profilesID=$id";
+  //SQL文を格納した文字列を定義(今回は指定レコードを削除するSQL文)
+  $sql = "delete from profiles where profilesID=$id";
 
-    //実行とその結果を受け取れる変数を用意
-    $query = $pdo_object -> prepare($sql);
+  //実行とその結果を受け取れる変数を用意
+  $query = $pdo_object -> prepare($sql);
 
-    //SQLを実行
-    $query -> execute();
+  //SQLを実行
+  $query -> execute();
 
-    //接続を切断
-    $pdo_object = null;
-    ?>
+  //接続を切断
+  $pdo_object = null;
+  ?>
 
   </body>
 </html>
