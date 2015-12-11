e611bf5693f0b0a9ee2f935c993ec658ffa8fcab
TakaHaru <xt.sakamoto@gmail.com>
2015-12-11 13:39:01 +0900

    (復習)提出

--- Challenge_7/7_1challenge.php
+++ Challenge_7/7_1challenge.php
@@ -5,28 +5,22 @@
 <meta charset="UTF-8">
       <title>7_1challenge</title>
 </head>
+    <!-- 課題1:Challenge_dbへの
+    エラーハンドリングを含んだ接続の確立を行ってください -->
   <body>
-<?php
+    <?php
+    //try-catchで接続エラーを取得＆表示
+    try{
+      $pdo_object=
+      new PDO('mysql:host=localhost;dbname=Challenge_db;charset=utf8',
+      'sakamoto',2591);
+    }catch(PDOException $E){
+      die('接続に失敗しました:'.$E->getMessage());
+    }
 
-/*
-課題1:Challenge_dbへの
-エラーハンドリングを含んだ接続の確立を行ってください
-*/
+    //接続を切断
+    $pdo_object = null;
+    ?>
 
-//try-catchで接続エラーを取得＆表示
-try{
-  $pdo_object=
-  new PDO('mysql:host=localhost;dbname=Challenge_db;charset=utf8',
-  'sakamoto',2591);
-}catch(PDOException $E){
-  die('接続に失敗しました:'.$E->getMessage());
-}
-
-
-//接続を切断
-$pdo_object = null;
-?>
-
-</body>
-
+  </body>
 </html>
