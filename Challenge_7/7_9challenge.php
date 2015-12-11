e0df51d26e413b42f382e27cc9905ae09574689b
TakaHaru <xt.sakamoto@gmail.com>
2015-12-11 13:46:12 +0900

    (復習)見えないスペース除去

--- Challenge_7/7_9challenge.php
+++ Challenge_7/7_9challenge.php
@@ -9,11 +9,15 @@
 
 <!--課題9:フォームからデータを挿入できる処理を構築してください。-->
 
+『profiles』テーブルに挿入したいデータを入力して下さい<br>
+
+<br>
+
 <form action="./7_9challenge.php" method="POST">
 profilesID:<br>
 <input type="text" name="profilesID" size=5><br>
 name:<br>
-<input type="text" name="name"><br>
+<input type="text" name="name"><br>
 tel:<br>
 <input type="text" name="tel"><br>
 age:<br>
@@ -24,63 +28,57 @@ birthday:<br>
   <input type="submit" name="btnSubmit" value="DBへ!!">
 </form>
 
-<?php
+  <?php
 
   //try-catchで接続エラーを取得＆表示
   try{
     $pdo_object=
     new PDO('mysql:host=localhost;dbname=Challenge_db;charset=utf8',
     'sakamoto',2591);
+    $pdo_object->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   }catch(PDOException $E){
     die('接続に失敗しました:'.$E->getMessage());
   }
 
-  if(isset ($_POST['profilesID'])){      //isset使って、「もし値が入っていれば」
-    $id = $_POST['profilesID'];           //変数に格納する
-  }else{
-    $id = null;
-  }
-  if(isset ($_POST['name'])){      //isset使って、「もし値が入っていれば」
+  //値が入っているかを確認し変数に格納
+  if(!empty($_POST['btnSubmit'])){
+  if(!empty ($_POST['profilesID']) && !empty ($_POST['name']) && !empty ($_POST['tel'])
+  && !empty ($_POST['age']) && !empty ($_POST['birth'])){
+    $id = $_POST['profilesID'];
     $name = $_POST['name'];
-  }else{
-    $name = null;
-  }
-  if(isset ($_POST['tel'])){      //isset使って、「もし値が入っていれば」
     $tel = $_POST['tel'];
-  }else{
-    $tel = null;
-  }
-  if(isset ($_POST['age'])){      //isset使って、「もし値が入っていれば」
     $age = $_POST['age'];
-  }else{
-    $age = null;
-  }
-  if(isset ($_POST['birth'])){      //isset使って、「もし値が入っていれば」
     $birth = $_POST['birth'];
-  }else{
-    $birth = null;
+
+    //SQL文を格納した文字列を定義(今回はレコード挿入のSQL文)
+    $sql = "insert into profiles(profilesID,name,tell,age,birthday)
+    values(:profilesID,:name,:tel,:age,:birth)";
+
+    //実行とその結果を受け取れる変数を用意
+    $query = $pdo_object -> prepare($sql);
+
+    //実数値はこのようにbindValueなどを用いて中身を入れ替える(安全のため)
+    $query -> bindValue(':profilesID',$id);
+    $query -> bindValue(':name',$name);
+    $query -> bindValue(':tel',$tel);
+    $query -> bindValue(':age',$age);
+    $query -> bindValue(':birth',$birth);
+
+    //SQLを実行(値が入っているか、変数に格納しvar_dumpで確認)
+    try{
+      $x= $query -> execute();
+      var_dump($x);
+    }catch(PDOException $E){
+      echo $E->getMessage();
+      var_dump($E);
+      }
+    }else{
+      echo '全ての項目を記入してください。';
+    }
   }
+    //接続を切断
+    $pdo_object = null;
 
-  $sql = "insert into profiles(profilesID,name,tel,age,birth)
-  values(:profilesID,:name,:tel,:age,:birth)";
-
-  $query = $pdo_object->prepare($sql);        //実行とその結果を受け取れる変数を用意
-
-  $query -> bindValue(':profilesID',$id);
-  $query -> bindValue(':name',$name);
-  $query -> bindValue(':tel',$tel);
-  $query -> bindValue(':age',$age);
-  $query -> bindValue(':birth',$birth);
-
-  $query -> execute();
-
-  var_dump($_POST);
-
-      //接続を切断
-      $pdo_object = null;
-
-    ?>
-
-</body>
-
+  ?>
+  </body>
 </html>
