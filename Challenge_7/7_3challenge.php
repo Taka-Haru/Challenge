da9ff5657cfff6d3c77fe413e6a97c89f3f328f1
TakaHaru <xt.sakamoto@gmail.com>
2015-12-11 13:40:04 +0900

    (復習)提出

--- Challenge_7/7_3challenge.php
+++ Challenge_7/7_3challenge.php
@@ -5,57 +5,55 @@
 <meta charset="UTF-8">
       <title>7_3challenge</title>
 </head>
+<!-- 課題3:前回の課題1で作成したテーブルからSELECT*により全件取得し、
+画面に取得した全情報を表示してください。 -->
   <body>
-<?php
+    <?php
 
-/*
-課題3:前回の課題1で作成したテーブルからSELECT*により全件取得し、
-画面に取得した全情報を表示してください。
-*/
+    //try-catchで接続エラーを取得＆表示
+    try{
+      $pdo_object=
+      new PDO('mysql:host=localhost;dbname=Challenge_db;charset=utf8',
+      'sakamoto',2591);
+    }catch(PDOException $E){
+      die('接続に失敗しました:'.$E->getMessage());
+    }
 
-//try-catchで接続エラーを取得＆表示
-try{
-  $pdo_object=
-  new PDO('mysql:host=localhost;dbname=Challenge_db;charset=utf8',
-  'sakamoto',2591);
-}catch(PDOException $E){
-  die('接続に失敗しました:'.$E->getMessage());
-}
+    //SQL文を格納した文字列を定義
+    $sql = "select * from profiles";
 
-/*
-SQL文を格納した文字列を定義(今回は全件表示のSQL文)
-(*文字列定義の為、"で括る必要がある。)
-*/
-$sql = "select * from profiles";
+    //実行とその結果を受け取れる変数を用意
+    $query = $pdo_object -> prepare($sql);
 
-/*
-実行とその結果を受け取れる変数を用意
-$SQLを実行＆結果を受け取る変数(ステートメントハンドラ) = $接続したオブジェクト -> prepare($SQL文);
-*/
-$query = $pdo_object -> prepare($sql);
+    //実数値はbindValueなどを用いて中身を入れ替える(安全の為)
+    $query -> bindValue(':profilesID',1);
+    $query -> bindValue(':name','田中広');
+    $query -> bindValue(':tell','0120-2110-2110');
+    $query -> bindValue(':age',27);
+    $query -> bindValue(':birthday','9838-8-10');
 
-//SQLを実行
- $query -> execute();
+    //SQLを実行
+    $query -> execute();
+    ?>
 
-/*
-データベースから取得した結果を使った処理をするには、fetchメソッドを使います。
-fetchメソッドの引数にPDO::FETCH_OBJを渡すと、オブジェクトとしてデータを取得できます。
-このオブジェクトは、フィールド名がプロパティになるので、
-profilesIDプロパティの値からbirthdayプロパティの値をechoで表示する。
-*/
-while( $row = $query->fetch(PDO::FETCH_OBJ) ){      //データベースから取得したデータを１つ持ってくる
- echo $row->profilesID . ': ';                     // profilesIDフィールドの内容を表示
- echo $row->name.'<br>';
- echo 'TEL： '.$row->tell.'<br>';
- echo 'AGE： '.$row->age.'<br>';
- echo 'BIRTH： '.$row->birthday.'<br>';            // birthdayフィールドの内容を表示
- echo '<br>';
-}
+    <!-- データベースから取得した結果を使った処理をするには、fetchメソッドを使います。
+    fetchメソッドの引数にPDO::FETCH_OBJを渡すと、オブジェクトとしてデータを取得できます。
+    このオブジェクトは、フィールド名がプロパティになるので、
+    profilesIDプロパティの値からbirthdayプロパティの値をそれぞれechoで表示する。 -->
+    <?PHP
+    
+    while($row = $query->fetch(PDO::FETCH_OBJ) ){      //データベースから取得したデータを１つ持ってくる
+     echo $row->profilesID . ': ';                      // profilesIDフィールドの内容を表示
+     echo $row->name.'<br>';                            // nameフィールドの内容を表示
+     echo 'TEL： '.$row->tell.'<br>';                   // tellフィールドの内容を表示
+     echo 'AGE： '.$row->age.'<br>';                    // ageフィールドの内容を表示
+     echo 'BIRTH： '.$row->birthday.'<br>';             // birthdayフィールドの内容を表示
+     echo '<br>';
+    }
 
-//接続を切断
-$pdo_object = null;
+    //接続を切断
+    $pdo_object = null;
 
-?>
-
+    ?>
   </body>
 </html>
