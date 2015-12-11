65c95be05c32e2803f4a63e583dbcd438290b013
TakaHaru <xt.sakamoto@gmail.com>
2015-12-11 13:40:45 +0900

    (復習)提出

--- Challenge_7/7_4challenge.php
+++ Challenge_7/7_4challenge.php
@@ -5,57 +5,43 @@
 <meta charset="UTF-8">
       <title>7_4challenge</title>
 </head>
+<!-- 課題4:前回の課題1で作成したテーブルからprofileID=1の情報を取得し、
+画面に取得した情報を表示してください -->
   <body>
-<?php
+    <?php
+    //try-catchで接続エラーを取得＆表示
+    try{
+      $pdo_object=
+      new PDO('mysql:host=localhost;dbname=Challenge_db;charset=utf8',
+      'sakamoto',2591);
+    }catch(PDOException $E){
+      die('接続に失敗しました:'.$E->getMessage());
+    }
 
-/*
-課題4:前回の課題1で作成したテーブルからprofileID=1の情報を取得し、
-画面に取得した情報を表示してください
-
-*/
-
-//try-catchで接続エラーを取得＆表示
-try{
-  $pdo_object=
-  new PDO('mysql:host=localhost;dbname=Challenge_db;charset=utf8',
-  'sakamoto',2591);
-}catch(PDOException $E){
-  die('接続に失敗しました:'.$E->getMessage());
-}
-
-/*
-SQL文を格納した文字列を定義(今回はレコード指定のSQL文)
-(*文字列定義の為、"で括る必要がある。)
-*/
-$sql = "select * from profiles where profilesID=1";
-
-/*
-実行とその結果を受け取れる変数を用意
-$SQLを実行＆結果を受け取る変数(ステートメントハンドラ) = $接続したオブジェクト -> prepare($SQL文);
-*/
-$query = $pdo_object -> prepare($sql);
-
-//SQLを実行
- $query -> execute();
-
- /*
- データベースから取得した結果を使った処理をするには、fetchメソッドを使います。
- fetchメソッドの引数にPDO::FETCH_OBJを渡すと、オブジェクトとしてデータを取得できます。
- */
- while( $row = $query->fetch(PDO::FETCH_OBJ) ){      //データベースから取得したデータを１つ持ってくる
-  echo $row->profilesID . ': ';                     // profilesIDフィールドの内容を表示
-  echo $row->name.'<br>';
-  echo 'TEL： '.$row->tell.'<br>';
-  echo 'AGE： '.$row->age.'<br>';
-  echo 'BIRTH： '.$row->birthday.'<br>';            // birthdayフィールドの内容を表示
-  echo '<br>';
- }
+    //SQL文を格納した文字列を定義(今回はレコード指定のSQL文)
+    $sql = "select * from profiles where profilesID=1";
 
 
-//接続を切断
-$pdo_object = null;
-?>
+    //実行とその結果を受け取れる変数を用意
+    $query = $pdo_object -> prepare($sql);
 
-</body>
+    //SQLを実行
+    $query -> execute();
+
+
+     while( $row = $query->fetch(PDO::FETCH_OBJ) ){      //データベースから取得したデータを１つ持ってくる
+      echo $row->profilesID . ': ';                      // profilesIDフィールドの内容を表示
+      echo $row->name.'<br>';                            // nameフィールドの内容を表示
+      echo 'TEL： '.$row->tell.'<br>';                   // tellフィールドの内容を表示
+      echo 'AGE： '.$row->age.'<br>';                    // ageフィールドの内容を表示
+      echo 'BIRTH： '.$row->birthday.'<br>';             // birthdayフィールドの内容を表示
+      echo '<br>';
+     }
+
+    //接続を切断
+    $pdo_object = null;
+    ?>
+
+  </body>
 
 </html>
