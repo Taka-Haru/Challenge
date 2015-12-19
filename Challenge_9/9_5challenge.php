c2b216b43e1b9bebc0eff7aee5ae0c7caf0b0d14
TakaHaru <xt.sakamoto@gmail.com>
2015-12-19 20:43:35 +0900

    (復習)追加変数消去

--- Challenge_9/9_5challenge.php
+++ Challenge_9/9_5challenge.php
@@ -8,21 +8,32 @@
   　　・２つの変数の中身をクリアするパブリックな関数
   -->
   <?php
+
   class Human{
 
     //パブリックな２つの変数を設定
     public $name;
     public $age;
+    public $from ='Japan';
+
 
     //２つの変数に値を設定するパブリックな関数
     public function profile($name,$age){
       $this->name = $name;
       $this->age = $age;
     }
-  }
+
+    //２つの変数の中身をechoするパブリックな関数
+    public function show(){
+      echo $this->name.'<br>';
+      echo $this->age.'<br>';
+    }
+  }   //Humanクラス定義終了
+
 
   //４のクラスを継承し中身のクリア処理
   class Teacher extends Human{
+    public $from = null;
   	public function clear_profile(){
   		$this->name = null;
   		$this->age = null;
@@ -31,7 +42,7 @@
 
   $y = new Teacher();
   $y->clear_profile();
-
+  echo $y->from.'<br>';
   ?>
   </body>
 </html>
