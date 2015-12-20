4e5470dc6a32372026d533226276d24aa9e55c3c
TakaHaru <xt.sakamoto@gmail.com>
2015-12-20 13:20:59 +0900

    (復習)検索方式

--- challenge_4/4_5challenge.php
+++ challenge_4/4_5challenge.php
@@ -5,17 +5,28 @@
 <meta charset="UTF-8">
       <title>4_5challenge</title>
 </head>
-  <body>
-    <!-- ５．自分の氏名について、バイト数と文字数を取得して、表示してください。 -->
-  <?php
+<!-- ５．自分の氏名について、バイト数と文字数を取得して、表示してください。 -->
+<body>
+  <form action="./4_5challenge.php" method="POST">
+    バイト数検索：<input type="text" name="baito">
+    <br>
+    文字数検索　：<input type="text" name="mojisu">
+    <input type="submit" name="btn" value="検索">
+    <br><br>
+  </form>
 
-  echo '『武田信玄』の名前のバイト数は：';
-  echo strlen ('武田信玄');
-  echo '<br>';
-  echo '『上杉謙信』の名前の文字数は：';
-  echo mb_strlen('上杉謙信');
-  echo '<br>';
+  <?php
+  if(!empty($_POST['btn'])){
+    if(!empty($_POST['baito']) && !empty($_POST['mojisu'])){
+      $x = $_POST['baito'];
+      $y = $_POST['mojisu'];
+      echo  '『'.$x.'』'.'のバイト数は：';
+      echo strlen($x).'バイトです。';
+      echo '<br>';
+      echo '『'.$y.'』'.'の文字数は　：';
+      echo mb_strlen($y).'文字です';
+    }
+  }
   ?>
 </body>
-
 </html>
