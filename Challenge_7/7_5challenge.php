e4bbce85fa09f2ccf5f7a2e6be90bad5fdb08a05
TakaHaru <xt.sakamoto@gmail.com>
2016-01-05 10:27:29 +0900

    (復習)foreachで復習

--- Challenge_7/7_5challenge.php
+++ Challenge_7/7_5challenge.php
@@ -19,7 +19,7 @@
     }
 
     //SQL文を格納した文字列を定義(今回は指定文字が含まれているレコード抽出のSQL文)
-    $sql = "select * from profiles where name like '%茂%'";
+    $sql = "select * from profiles where name like '%松%'";
 
     //実行とその結果を受け取れる変数を用意
     $query = $pdo_object -> prepare($sql);
@@ -27,15 +27,22 @@
     //SQLを実行
     $query -> execute();
 
-    while( $row = $query->fetch(PDO::FETCH_OBJ) ){      //データベースから取得したデータを１つ持ってくる
-     echo $row->profilesID . ': ';                      // profilesIDフィールドの内容を表示
-     echo $row->name.'<br>';                            // nameフィールドの内容を表示
-     echo 'TEL： '.$row->tell.'<br>';                   // tellフィールドの内容を表示
-     echo 'AGE： '.$row->age.'<br>';                    // ageフィールドの内容を表示
-     echo 'BIRTH： '.$row->birthday.'<br>';             // birthdayフィールドの内容を表示
-     echo '<br>';
+    foreach ($query->fetchAll(PDO::FETCH_ASSOC) as $key){
+      foreach ($key as $value){
+        //var_dump($value);
+        echo $value.'<br>';
+      }
     }
 
+    // while($row = $query->fetch(PDO::FETCH_OBJ) ){      //データベースから取得したデータを１つ持ってくる
+    //  echo $row->profilesID . ': ';                      // profilesIDフィールドの内容を表示
+    //  echo $row->name.'<br>';                            // nameフィールドの内容を表示
+    //  echo 'TEL： '.$row->tell.'<br>';                   // tellフィールドの内容を表示
+    //  echo 'AGE： '.$row->age.'<br>';                    // ageフィールドの内容を表示
+    //  echo 'BIRTH： '.$row->birthday.'<br>';             // birthdayフィールドの内容を表示
+    //  echo '<br>';
+    // }
+    
     //接続を切断
     $pdo_object = null;
     ?>
